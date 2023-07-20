d38fa2388348fe9fe430bd463584b005e562dedd
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-20 14:05:11 +0100

    navlinks and design for navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,8 +24,22 @@ const Navbar = () => {
       <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> 
       <p className="text-white text-[18px] font-bold cursor-pointer">Sefeoluwa <span className="sm:block hidden">Akinbeye</span>
       </p>
-      </Link>
-      <p className="text-red-500">adsds</p>
+      </Link>     
+      <ul className="list-none hidden sm:flex flex-row gap-10">
+        {navLinks.map((Link) => ( 
+          <li
+          key={Link.id}
+          className={`${
+            active === Link.title ? "text-white"
+            : "text-secondary"
+          }
+          hover:text-white text-[18px] font-medium cursor-pointer`}
+          onClick={() => setActive(Link.title)}
+          >
+            <a href={`#${Link.id}`}>{Link.title}</a>
+          </li>
+        ))}
+      </ul>
       
     </div>
     </nav>
