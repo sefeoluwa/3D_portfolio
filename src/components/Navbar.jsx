d5dbf9ea5ad2a173f00759829ecbd2ba02edc9fa
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-20 11:20:30 +0100

    Navbar alignment set :fire:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 
-import { styles } from '/home/yisola/repos/3D_portfolio/styles.js';
-import { navLinks } from '../constants/index';
-import { logo, menu, close } from '../assets';
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
     const [active, setActive] = useState("")
 
   return (
     <nav
-    className={` ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary `} >
+    className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
 
     <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-      <Link to="/" className="flex items-center gap-2" onClick={
-        () => {
+      <Link 
+      to="/" 
+      className="flex items-center gap-2" 
+      onClick={() => {
           setActive("");
           window.scrollTo(0,0)
         }
       }>
-      <img src={logo} alt="log" className='w-9 h-9 object-contain'/>
-
+      <img src={logo} alt="logo" className="w-9 h-9 object-contain" /> 
+      <p className="text-white text-[18px] font-bold cursor-pointer">Sefeoluwa <span className="sm:block hidden">Akinbeye</span>
+      </p>
       </Link>
+      <p className="text-red-500">adsds</p>
+      
     </div>
     </nav>
   )
