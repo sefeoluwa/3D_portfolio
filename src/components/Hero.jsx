284d690d995c443d7408a5c2518feb859cf541a7
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-20 23:47:29 +0100

    frmaer motion to navigate to about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 import { motion } from 'framer-motion';
-
 import { styles } from '../styles'
 import { ComputersCanvas } from './canvas'
 
@@ -17,10 +16,28 @@ const Hero = () => {
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Sefeoluwa</span> </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop modern websites <br className="sm:block hidden " /> for exceptional user experiences.</p>
         </div>
+        </div>
 
-        
-        <ComputersCanvas />
-      </div>
+        {/* <ComputersCanvas /> */}
+
+        <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+          <a href="#about">
+            <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+              <motion.dev 
+              animate={{
+                y:[0, 24, 0]
+              }}
+              transition={{
+                duration:1.5,
+                repeat:Infinity,
+                repeatType:'loop'
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+               />
+            </div>
+          </a>
+        </div>
+     
     </section>
   )
 }
