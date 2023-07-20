4f5fc363dd76f50dbf0d7767e62ee35f7c2ce131
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-20 14:23:40 +0100

    hamburger menu base design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-    const [active, setActive] = useState("")
+    const [active, setActive] = useState("");
+     const [toggle, setToggle] = useState(false);
 
   return (
     <nav
@@ -30,10 +31,8 @@ const Navbar = () => {
           <li
           key={Link.id}
           className={`${
-            active === Link.title ? "text-white"
-            : "text-secondary"
-          }
-          hover:text-white text-[18px] font-medium cursor-pointer`}
+            active === Link.title ? "text-white" : "text-secondary"
+          }hover:text-white text-[18px] font-medium cursor-pointer`}
           onClick={() => setActive(Link.title)}
           >
             <a href={`#${Link.id}`}>{Link.title}</a>
@@ -41,6 +40,13 @@ const Navbar = () => {
         ))}
       </ul>
       
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img src={ toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer"  onClick={() => setToggle(!toggle)} />
+         <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl `} >
+          
+         </div>
+      </div>
+
     </div>
     </nav>
   )
