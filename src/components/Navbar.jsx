740e63af14b5cd6668bdf1742196cdba2961a475
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-22 19:06:58 +0100

    bg-transparent on scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React, {useEffect, useState} from "react";
 import { Link } from 'react-router-dom';
 
@@ -10,7 +11,12 @@ const Navbar = () => {
      const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav
+    className={`${
+      styles.paddingX
+    } w-full flex items-center py-5 fixed top-0 z-20 ${
+      scrolled ? "bg-primary" : "bg-transparent"
+    }`}>
 
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
