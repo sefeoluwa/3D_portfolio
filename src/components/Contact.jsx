b9bf63c17256d4ebdb2c7b41311e469d136adcdb
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-22 18:14:58 +0100

    handleChange function

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,9 @@ import { EarthCanvas } from './canvas'
 import { SectionWrapper } from '../hoc'
 import { slideIn } from '../utils/motion'
 
+// template_uaevb3s
+// service_0dviduv
+// Key  n5kx•••••••••••••••••
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -17,9 +20,14 @@ const Contact = () => {
   })
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({...form, [name]: value})
+  }
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
   return (
     <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden'>
@@ -43,7 +51,7 @@ const Contact = () => {
         <textarea rows='7' name='message' value={form.message} onChange={handleChange} placeholder="What do you want to say?" className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' />
       </label>
 
-        <button type='submit' className='bg-tertiary py-3 px-3 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'>{loading ? 'Sending...' : 'Send'}</button>
+        <button type='submit' className='bg-tertiary py-3 px-3 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'> {loading ? 'Sending...' : 'Send'}</button>
       </form>
       </motion.div>
 
