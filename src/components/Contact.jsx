210d1b3654d2bc2e095ffe671ed7907ceacb0a14
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-22 12:51:41 +0100

    commented out earth canvas for now

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -48,7 +48,7 @@ const Contact = () => {
       </motion.div>
 
       <motion.div variants={slideIn('right', 'tween', 0.2, 1)} className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]">
-        <EarthCanvas />
+        {/* <EarthCanvas /> */}
       </motion.div>
     </div>
   )
