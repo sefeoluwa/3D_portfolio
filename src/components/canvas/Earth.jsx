8ad933dbfe1ce6dc1f28d6ee925c7e55b7fe0ba2
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-22 12:04:26 +0100

    OrbitControls and CanvasLoader

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader'
+import { Suspense } from 'react';
 
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+    shadows
+    frameloop='demand'
+    gl={{ preserveDrawingBuffer: true }}
+    camera={{ }}
+    >
+    <Suspense fallback={<CanvasLoader />}
+    <OrbitControls />
+    </Suspense>
+
+    </Canvas>
   )
 }
 
