edfeee13acb4d19b31fd07a4cc1320d7cf3e765b
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-22 20:08:49 +0100

    added description to metalverse project

--- src/constants/index.js
+++ src/constants/index.js
@@ -135,38 +135,11 @@ import {
     },    
   ];
   
-  const testimonials = [
-    {
-      testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-  ];
-  
   const projects = [
     {
       name: "MetalVerse",
       description:
-        "A base landing page with registration forms validated with javascript.",
+        "A modern landing page with registration forms validated with javascript. It contains a sign-up page, and a ssign in page",
       tags: [
         {
           name: "css",
@@ -249,5 +222,5 @@ import {
     },
   ];
   
-  export { services, technologies, experiences, testimonials, projects };
+  export { services, technologies, experiences, projects };
   
