6201bb26780cde001ff9fc0f3d5104392b02c369
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-23 15:35:40 +0100

    z-index edits

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -59,8 +59,8 @@ const Contact = () => {
   }
 
   return (
-    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 z-1 overflow-hidden'>
-      <motion.div variants={slideIn('left', 'tween', 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl' >
+    <div className='xl:mt-12 xl:flex-row flex-col-reverse flex gap-10  overflow-hidden'>
+      <motion.div variants={slideIn('left', 'tween', 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl z--50' >
       <p className={styles.sectionSubText}>Get in touch</p>
       <h3 className={styles.sectionHeadText}>Contact.</h3>
 
