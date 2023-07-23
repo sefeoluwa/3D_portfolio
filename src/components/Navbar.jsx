a17448cae58a06f600905b727802620049491362
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-23 14:43:05 +0100

    background color changed to hero backgorund color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     <nav
     className={`${
       styles.paddingX
-    } w-full flex items-center py-5 fixed top-0 z-50 bg-black`}>
+    } w-full flex items-center py-5 fixed top-0 z-50 bg-hero-pattern`}>
 
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link 
