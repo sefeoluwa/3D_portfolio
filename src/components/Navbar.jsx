6d9cfb6b78bc51b0c61d84847d66f6e0b49e6432
sefeoluwa <sefeoluwa01@gmail.com>
2023-07-23 13:18:41 +0100

    z-index for navbar increased

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import React, {useEffect, useState} from "react";
+import React, { useState} from "react";
 import { Link } from 'react-router-dom';
 
 import { styles } from "../styles";
@@ -10,28 +10,13 @@ import { logo, menu, close } from "../assets";
 const Navbar = () => {
     const [active, setActive] = useState("");
      const [toggle, setToggle] = useState(false);
-     const [scrolled, setScrolled] = useState(false);
-
-     useEffect(() => {
-       const handleScroll = () => {
-         const scrollTop = window.scrollY;
-         if (scrollTop > 100) {
-           setScrolled(true);
-         } else {
-           setScrolled(false);
-         }
-       };
-   
-       window.addEventListener("scroll", handleScroll);
-   
-       return () => window.removeEventListener("scroll", handleScroll);
-     }, []);
+     
 
   return (
     <nav
     className={`${
       styles.paddingX
-    } w-full flex items-center py-5 fixed top-0 z-20 ${
+    } w-full flex items-center py-5 fixed top-0 z-50 ${
       scrolled ? "bg-primary" : "bg-transparent"
     }`}>
 
