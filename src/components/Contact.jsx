1ca7cedc73e031e004c1f9e1e2de5934fa4d5d22
sefeoluwa <sefeoluwa01@gmail.com>
2023-08-09 13:29:59 +0100

    regex to validate email address added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react-refresh/only-export-components */
-import { useState, useRef } from 'react'
-import { motion } from 'framer-motion'
-import emailjs from '@emailjs/browser'
-import  { styles } from '../styles'
-import { EarthCanvas } from './canvas'
-import { SectionWrapper } from '../hoc'
-import { slideIn } from '../utils/motion'
-
+import  { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
 
 const Contact = () => {
   const formRef = useRef();
@@ -15,20 +12,30 @@ const Contact = () => {
     name: '',
     email: '',
     message: '',
-  })
+  });
+
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const [loading, setLoading] = useState(false);
+  const [sendButton, setSendButton] = useState('Send');
+  const [isValidEmail, setIsValidEmail] = useState(true);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setForm({...form, [name]: value})
-  }
+    setForm({ ...form, [name]: value });
+    setIsValidEmail(true); // Reset validation message when user types
+  };
 
-  const [loading, setLoading] = useState(false);
-  const [sendButton, setSendButton] = useState('Send'); // Add the state for the button text
-
-   const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-    setSendButton('Sending...'); // Show "Sending..." when clicked
+    setSendButton('Sending...');
+
+    if (!emailRegex.test(form.email)) {
+      setIsValidEmail(false);
+      setLoading(false);
+      setSendButton('Send');
+      return;
+    }
 
     emailjs
       .send(
@@ -45,11 +52,10 @@ const Contact = () => {
       )
       .then(() => {
         setLoading(false);
-        setSendButton('Sent'); // Change the button text to "Sent" after successful sending
-
+        setSendButton('Sent');
         setTimeout(() => {
-          setSendButton('Send'); // Revert back to "Send" after a short delay
-        }, 2000); // 2 seconds delay
+          setSendButton('Send');
+        }, 2000);
         setForm({
           name: '',
           email: '',
@@ -58,12 +64,10 @@ const Contact = () => {
       })
       .catch((error) => {
         setLoading(false);
-        setSendButton('Error'); // Change the button text to "Error" in case of an error
-
+        setSendButton('Error');
         setTimeout(() => {
-          setSendButton('Send'); // Revert back to "Send" after a short delay
-        }, 2000); // 2 seconds delay
-
+          setSendButton('Send');
+        }, 2000);
         console.log(error);
         alert('Snap! Something went wrong. Please resend your message');
       });
@@ -76,19 +80,31 @@ const Contact = () => {
       <h3 className={styles.sectionHeadText}>Contact.</h3>
 
       <form ref={formRef} onSubmit={handleSubmit} className='mt-12 flex  flex-col gap-8'>
-      <label htmlFor="" className="flex flex-col">
+      <label htmlFor="name" className="flex flex-col">
         <span className='text-white font-medium mb-4'>Your Name</span>
-        <input type="text" name='name' value={form.name} onChange={handleChange} placeholder="What's your name?" className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium' />
+        <input type="text" name='name' id='name' value={form.name} onChange={handleChange} placeholder="What's your name?" required className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium' />
       </label>
 
-      <label htmlFor="" className="flex flex-col">
-        <span className='text-white font-medium mb-4'>Your Email</span>
-        <input type="text" name='email' value={form.email} onChange={handleChange} placeholder="Your email" className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium' />
-      </label>
+      <label htmlFor="email" className="flex flex-col">
+            <span className='text-white font-medium mb-4'>Your Email</span>
+            <input
+              type="text"
+              name='email'
+              id='email'
+              value={form.email}
+              onChange={handleChange}
+              placeholder="Your email"
+              required
+              className={`bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outlined-none border-none font-medium ${!isValidEmail ? 'border-red-500' : ''}`}
+            />
+            {!isValidEmail && (
+              <span className="text-red-500">Invalid email format</span>
+            )}
+          </label>
 
       <label htmlFor="" className="flex flex-col">
         <span className='text-white font-medium mb-4'>Your Message</span>
-        <textarea rows='7' name='message' value={form.message} onChange={handleChange} placeholder="What do you want to say?" className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' />
+        <textarea rows='7' name='message' value={form.message} onChange={handleChange} placeholder="What do you want to say?" required className='bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium' />
       </label>
 
         <button type='submit' className='bg-tertiary py-3 px-3 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl'>{loading ? 'Sending...' : sendButton}</button>
